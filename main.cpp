fc632fd130a9e410d280c17011cbcf4d085c128b
SrLeo0770 <bmartinezd3@miumg.edu.gt>
2024-02-24 10:02:25 -0600

    ejercicio

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+int main() {
+    int numero;
+    cout << "INGRESE UN NUMERO Y LE DIRE SI ES PAR O IMPAR: " << endl;
+    cin >> numero;
+
+
+    if (numero%2 ==0){
+        cout<<numero<<" ES UN NUMERO PAR"<<endl;
+    }else {
+        cout<<numero<<" ES UN NUMERO IMPAR"<<endl;
+    }
+
+
+    return 0;
+}
